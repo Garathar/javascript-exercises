5c287f401d2e8c2211eb5a79f81488a9b6456b50
Garathar <garatarganis@gmail.com>
2026-03-23 14:33:47 +0100

    Modify the js to fulfil the request
    It sum up to positive integer

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(startingNum,endingNum) {
+    if (Number.isInteger(startingNum) == false || 
+        startingNum < 0) return "ERROR";
+    if (Number.isInteger(endingNum) == false || 
+        endingNum < 0) return "ERROR";
+    let temp = 0;
+    let smallerNum = 0;
+    let biggerNum = 0;
+    if (startingNum <= endingNum){
+        smallerNum = startingNum
+        biggerNum = endingNum
+    }
+    else{
+        smallerNum = endingNum
+        biggerNum = startingNum
+    }
+    for (let i = smallerNum; i <= biggerNum;i++){
+        temp += i
+    }
+    return temp;
 };
-
+console.log(sumAll(10,2))
 // Do not edit below this line
 module.exports = sumAll;
