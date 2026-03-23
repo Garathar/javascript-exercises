abc7f13c985f1b652989127f08821fd5a43eb0c9
Garathar <garatarganis@gmail.com>
2026-03-23 11:51:42 +0100

    Modify js to the specification of the task

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr,...args) {
+    let remove = [];
+    for (let i = 1; i <= arguments.length-1;i++){
+        remove.push(arguments[i])
+    }
+    function removal(item){
+        for (let i = 0; i <= remove.length-1;i++){
+            if (item === remove[i]) return false;
+        }
+        return true;
+    }
+    let temp = arr.filter(removal)
+    return temp
 
+    
+};
 // Do not edit below this line
 module.exports = removeFromArray;
