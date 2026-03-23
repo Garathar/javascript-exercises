1ecb918f2ccd9d64d8e7ec556be88062711b9478
Garathar <garatarganis@gmail.com>
2026-03-23 19:52:52 +0100

    Modify js to fulfil request
    Both converters work, allowing only one decimal

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,9 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+  return +((number - 32) / 1.8).toFixed(1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+  return +(number * 1.8 + 32).toFixed(1)
 };
-
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
