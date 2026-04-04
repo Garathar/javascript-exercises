267d89e3a013b183c22f43451dff99d1da26c615
Garathar <garatarganis@gmail.com>
2026-04-04 19:56:47 +0200

    Modify the files to fullfil the request

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const sum = arr.reduce((sum,current) => sum+current,0);
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  const multi = arr.reduce((obj,current) => obj * current,1);
+  return multi
 };
 
-const power = function() {
-	
+const power = function(base,exponent) {
+	return Math.pow(base,exponent)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let temp = 1;
+  for (let i = num;i > 1;i--){
+    temp *= i
+  };
+  return temp
 };
 
 // Do not edit below this line
