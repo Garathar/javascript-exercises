1a0fbc8f6944ae680b076652cd154ee80de593b6
Garathar <garatarganis@gmail.com>
2026-04-06 18:17:27 +0200

    Modify files according to the request

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const old = arr.reduce((oldest,currant) => {  
+        if (!oldest.yearOfDeath){
+            oldest.yearOfDeath = (new Date()).getFullYear();
+        }
+        if (!currant.yearOfDeath){
+            currant.yearOfDeath = (new Date()).getFullYear();
+        }
+        let old = currant.yearOfDeath-currant.yearOfBirth
+        if (old > (oldest.yearOfDeath-oldest.yearOfBirth))return oldest=currant
+        return oldest
+    })
+    return old
 
 };
 
