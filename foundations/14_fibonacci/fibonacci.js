57baa9ffc9c619f83feec80af10751636fa9f6bc
Garathar <garatarganis@gmail.com>
2026-04-06 17:05:38 +0200

    Modify the files according to the task

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(count) {
+    let counter = parseInt(count)
+    if (counter < 0)return"OOPS"
+    let arr = [0,1]
+    if (counter < 2)return arr[counter]
+    for (let i = 2;i <= counter;i++){
+        arr.push(arr[i-1]+arr[i-2])
+    }
+    return arr[counter]
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
